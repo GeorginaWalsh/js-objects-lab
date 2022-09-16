85d831f5f84a03d07e5ae757cb43976ebc148ce5
georginawalsh <20093147>
2022-09-16 15:26:43 +0100

    Arrays.

--- fundamentals.js
+++ fundamentals.js
@@ -1,5 +1,10 @@
 const aCar = {
     owner : "Joe Bloggs",
+    address : '3 Walkers Lane',
+    previous_owners: [
+        {name: 'Pat Smith', address: '1 Main Street'},
+        {name: 'Sheila Dwyer', address: '2 High Street'},
+      ],
     type : {
         make: "Toyota",
         model: "Corolla",
@@ -32,5 +37,11 @@ console.log(
     aCar.mileage + 
     ' mileage, with ' +
     aCar.colour.interior.texture + 
-    ' interior.')
+    ' interior.');
+
+    console.log(
+        'First owner : ' + 
+        aCar.previous_owners[0].name +
+        ' - ' +
+        aCar.previous_owners[0].address );
 
